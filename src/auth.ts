a62d2266ea75532ab5ca67a831be62f686de112b
sajid-k <63906063+sajid-k@users.noreply.github.com>
2026-05-17 05:26:31 +0530

    Beta v0.1.2

--- src/auth.ts
+++ src/auth.ts
@@ -6,12 +6,14 @@ import GoogleProvider from "next-auth/providers/google"
 import bcrypt from "bcryptjs"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  debug: true,
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     CredentialsProvider({
       name: "Credentials",
